2c444dfcc8a5523ab7ec9b663e4bd689dd233222
headweek <abdullayev233@gmail.com>
2023-11-08 23:18:31 +0400

    Initial Commit

---
+++ m2hw1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m2hw1
+//
+//  Created by Salman Abdullayev on 08.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
